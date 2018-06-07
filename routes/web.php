93ba6734ca38ccb0102cff136ef2e3e2dda020bc
Yuki Miyasaka <yuki.m5911@gmail.com>
2018-06-07 07:59:22 +0000

    post1

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -22,4 +20,5 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 Route::group(['middleware' => ['auth']], function () {
 Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
