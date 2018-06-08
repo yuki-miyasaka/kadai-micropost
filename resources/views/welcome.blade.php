68a966dae87ab6ac8e162c306b1c806adf2dcb93
Yuki Miyasaka <yuki.m5911@gmail.com>
2018-06-08 01:27:08 +0000

    twitterclone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,6 @@
 
 @section('content')
     @if (Auth::check())
-        <?php $user = Auth::user(); ?>
-        {{ $user->name }}
-    @else
-        <div class="center jumbotron">
-            <div class="text-center">
-                <h1>Welcome to the Microposts</h1>
-                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
-            </div>
-        </div>
-    @endif
-@endsection@section('content')
-    @if (Auth::check())
         <div class="row">
             <aside class="col-md-4">
             </aside>
