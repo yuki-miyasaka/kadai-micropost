bc0f376674078969eeb1190588e2304eac216281
Yuki Miyasaka <yuki.m5911@gmail.com>
2018-06-15 03:42:39 +0000

    timeline

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -15,7 +15,7 @@
                      @if (Auth::check())
                         <li>{!! link_to_route('users.index', 'Users') !!}</li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <a href="/" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">My profile</a></li>
                                 <li role="separator" class="divider"></li>
